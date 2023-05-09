bac6479fecc1313db1d98a30b5ad518e12681a6a
JB <tonymoooon543@gmail.com>
2023-05-09 08:50:48 -0700

    Add files via upload

--- filemaker.py
+++ filemaker.py
@@ -23,9 +23,17 @@ class {FILE}(commands.Cog):
     def __init__(self, bot: discord.Bot):
         self.bot = bot
     
+    ##############
+    #  Commands  #
+    ##############
+
     @commands.slash_command()
     async def ping(self, ctx: discord.ApplicationContext):
         await ctx.respond("Hello World!")
+
+    ###########
+    #  Tasks  #
+    ###########
 
 def setup(bot: discord.Bot):
     bot.add_cog({FILE}(bot))
--- main.py
+++ main.py
@@ -6,12 +6,6 @@ bot = discord.Bot()
 async def on_ready():
     print("Ready")
 
-@bot.slash_command()
-async def ping(ctx: discord.ApplicationContext):
-    await ctx.respond("Pong!")
-
-#bot.load_extension('cogs.levels')
 bot.load_extension('cogs.weather')
-
 
 bot.run("")
